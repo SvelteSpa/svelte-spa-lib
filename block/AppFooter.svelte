f5efec562ae5d181eae3b858890076e0d1221f0c
Axl Eu <axl@a5x.eu>
2021-09-05 17:59:49 +0200

    AppFooter has a single <slot>

--- block/AppFooter.svelte
+++ block/AppFooter.svelte
@@ -1,13 +1,7 @@
 <script context="module" lang="ts">
-  import type { bool } from '@spa/typs'
 </script>
 
 <script lang="ts">
-  export let expand: bool
 </script>
 
-{#if expand}
-  2
-{:else}
-  1
-{/if}
+<slot />
